9b72edb9e6f9a84dc309f54cae6e38eb4acaa5e3
cqr530 <1154927938@qq.com>
2020-12-23 17:15:43 +0800

    2020.12.23 创建

---
+++ src/main/java/com/lianxi/drugs/DrugsApplication.java
@@ -0,0 +1,13 @@
+package com.lianxi.drugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugsApplication.class, args);
+    }
+
+}
