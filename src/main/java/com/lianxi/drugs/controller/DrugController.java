1fbd2e43dc301eb84d3e81279046c23be4d6f68a
cqr530 <1154927938@qq.com>
2020-12-24 14:24:20 +0800

    2020.12.24 修改controller

--- src/main/java/com/lianxi/drugs/controller/DrugController.java
+++ src/main/java/com/lianxi/drugs/controller/DrugController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController("/drugs")
 public class DrugController {
     @Autowired
     private com.lianxi.drugs.service.DrugSystemService drugSystemService;
