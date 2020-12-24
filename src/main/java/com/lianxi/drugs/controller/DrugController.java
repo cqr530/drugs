00cecea674475f0a89553ddc4482904a098580b1
cqr530 <1154927938@qq.com>
2020-12-24 14:41:38 +0800

    2020.12.24 修改controller

--- src/main/java/com/lianxi/drugs/controller/DrugController.java
+++ src/main/java/com/lianxi/drugs/controller/DrugController.java
@@ -6,21 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/drugs")
+@RestController
+@RequestMapping("/drugs")
 public class DrugController {
     @Autowired
     private com.lianxi.drugs.service.DrugSystemService drugSystemService;
 
 
     /**
-     *
+     *2020.12.24 陈泉润
      * @param id
      * @param pwd
      * @return 登录接口
      */
     @RequestMapping("/denglu")
-    @ResponseBody
     public int finduser(@RequestParam("id")Integer id,@RequestParam("pwd") String pwd){
         return drugSystemService.queryuser(id, pwd);
     }
