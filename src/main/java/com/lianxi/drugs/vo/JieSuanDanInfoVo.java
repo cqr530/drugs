f399f8c24db7a39c7c09d072cf8cd5e0aba6c2e4
zmh123456789 <2879442908@qq.com>
2021-01-22 17:39:35 +0800

    2021-1-22 结算单info查询分页前后端

--- src/main/java/com/lianxi/drugs/vo/JieSuanDanInfoVo.java
+++ src/main/java/com/lianxi/drugs/vo/JieSuanDanInfoVo.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
@@ -17,28 +18,70 @@ public class JieSuanDanInfoVo {
     /*名字*/
     private String payoffTabName;
 
-    /*联系人*/
-    private String contacts;
+    /*医院名字*/
+    private String hospitalName;
 
-    /*联系电话*/
-    private String contactsPhone;
+    /*流水号*/
+    private Integer serialNum;
+
+    /*通用名*/
+    private String genericName;
+
+    /*中标价*/
+    private BigDecimal tenderPrice;
+
+    /*交易价*/
+    private BigDecimal orderTradingPrice;
+
+    /*采购量*/
+    private Integer orderBuyCount;
+
+    /*采购金额*/
+    private BigDecimal orderBuyPrice;
+
+    /*药品批号*/
+    private BigDecimal approvalNum;
+
+    /*药品有效期*/
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date approvalNumDate;
+
+    /*剂型*/
+    private String reagentType;
+
+    /*规格*/
+    private String drugSize;
+
+    /*单位*/
+    private String company;
+
+    /*转换系数*/
+    private Integer transverNum;
+
+    /*质量层次*/
+    private String qualityLevel;
+
+    /*生产企业*/
+    private String factoryName;
+
+    /*供货企业*/
+    private String companyName;
 
     /*创建时间*/
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
-    private Date createTime;
+    private Date startTime;
 
     /*提交时间*/
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
-    private Date inputTime;
+    private Date endTime;
 
-    /*备注*/
-    private String payoffTabPostil;
+    /*采购状态*/
+    private String drugTranlStatus;
 
-    /*状态*/
-    private String payoffTabStatus;
 
-    /*总金额*/
-    private String payoffPrice;
+    private String userCompany;
+
 }
